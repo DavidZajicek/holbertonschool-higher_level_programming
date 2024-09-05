89fb13d1fb8f683d7b1f74de844cdbcd736aa59d
David Zajicek <david.a.zajicek@outlook.com>
2024-09-05 12:45:55 +1000

    I'm so tired

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,8 +4,9 @@ def fizzbuzz():
     for i in range(101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz".format(), end=" ")
-        if i % 3 == 0 and i % 5 != 0:
+        elif i % 3 == 0 and i % 5 != 0:
             print("Fizz".format(), end=" ")
-        if i % 3 != 0 and i % 5 == 0:
+        elif i % 3 != 0 and i % 5 == 0:
             print("Buzz".format(), end=" ")
-        print("{}".format(i), end=" ")
+        else:
+            print("{}".format(i), end=" ")
