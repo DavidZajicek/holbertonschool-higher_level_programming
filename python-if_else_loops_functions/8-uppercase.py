c0ad1c730099f2433904cdbe9d6362441d497136
David Zajicek <david.a.zajicek@outlook.com>
2024-09-05 12:23:10 +1000

    str to str

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@
 def uppercase(string: str):
     for c in string:
         if ord(c) >= 97 and ord(c) <= 122:
-            c -= 32
+            c = chr(ord(c) - 32)
     print(string.format(string))
