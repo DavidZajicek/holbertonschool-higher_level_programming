ac8e048801417519250f32066c53c73286d8f645
David Zajicek <david.a.zajicek@outlook.com>
2024-09-05 12:45:24 +1000

    fix: forgot to print the numbers lol

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,7 +7,5 @@ def fizzbuzz():
         if i % 3 == 0 and i % 5 != 0:
             print("Fizz".format(), end=" ")
         if i % 3 != 0 and i % 5 == 0:
-            if i == 100:
-                print("Buzz".format())
-            else:
-                print("Buzz".format(), end=" ")
+            print("Buzz".format(), end=" ")
+        print("{}".format(i), end=" ")
