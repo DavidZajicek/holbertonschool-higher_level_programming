132768913988c70fd52b819267f00aa000e00513
David Zajicek <david.a.zajicek@outlook.com>
2024-09-05 12:36:27 +1000

    fix: abs

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    if number < 0:
-        print("{}".format(number % -10), end="")
-        return number % -10
-    elif number > 0:
+    number = abs(number)
+    if number > 0:
         print("{}".format(number % 10), end="")
         return number % 10
     print("0".format(), end="")
