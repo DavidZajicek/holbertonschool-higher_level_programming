2abbf68f831f4f35c6d81fa2fe83c0cd9c21a859
David Zajicek <david.a.zajicek@outlook.com>
2024-09-11 12:07:46 +1000

    0-print_list_integer.py

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(my_list[i]))
+
+
+def main():
+    print_list_integer()
+
+
+if __name__ == "__main__":
+    main()
