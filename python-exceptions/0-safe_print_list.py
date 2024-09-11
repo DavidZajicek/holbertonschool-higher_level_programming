1f8ddc4bdd2241a36b7cad73c20fd4b6a6f43947
David Zajicek <david.a.zajicek@outlook.com>
2024-09-11 20:57:48 +1000

    oof

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -12,16 +12,17 @@ def safe_print_list(my_list=[], x=0):
             return i
         except TypeError:
             return i
-    print("")
+    if i > 0:
+        print("")
     return i
 
 
 def main():
     my_list = [1, 2, 3, 4, 5]
 
-    nb_print = safe_print_list(my_list, -2)
+    nb_print = safe_print_list(my_list, 1)
     print("nb_print: {:d}".format(nb_print))
-    nb_print = safe_print_list(my_list, len(my_list))
+    nb_print = safe_print_list([], len([]))
     print("nb_print: {:d}".format(nb_print))
     nb_print = safe_print_list(my_list, len(my_list) + 2)
     print("nb_print: {:d}".format(nb_print))
