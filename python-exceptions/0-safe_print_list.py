95edae68f70a91d99f0931feb1b1f6d970c0f717
David Zajicek <david.a.zajicek@outlook.com>
2024-09-11 20:34:15 +1000

    Exception? I assume this will yell at me, I would

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             i += 1
-        except (IndexError, TypeError):
+        except (Exception):
             print("")
             return i
     print("")
@@ -14,7 +14,7 @@ def safe_print_list(my_list=[], x=0):
 
 
 def main():
-    my_list = 3
+    my_list = [1, 2, 3, 4, 5]
 
     nb_print = safe_print_list(my_list, 2)
     print("nb_print: {:d}".format(nb_print))
