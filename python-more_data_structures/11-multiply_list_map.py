761b9b8f318ed11ff3d6fe625c900e67f486a9f3
David Zajicek <david.a.zajicek@outlook.com>
2024-09-11 17:04:20 +1000

    omfg why 3 lines go away

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
-
-
 def multiply_list_map(my_list: list = [], number=0):
     return list(map(lambda x: x*number, my_list)).copy()
-
-
-def main():
-    my_list = [1, 2, 3, 4, 6]
-    new_list = multiply_list_map(my_list, 4)
-    print(new_list)
-    print(my_list)
-
-
-if __name__ == "__main__":
-    main()
