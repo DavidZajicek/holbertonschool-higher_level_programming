04b978309ef4c98e6d351bc06d8cd223b72302c2
David Zajicek <david.a.zajicek@outlook.com>
2024-09-11 16:11:53 +1000

    line length

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -6,7 +6,8 @@ def print_sorted_dictionary(a_dictionary: dict):
 
 
 def main():
-    a_dictionary = {'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3]}
+    a_dictionary = {'language': "C", 'Number': 89, 'track': "Low level",
+                    'ids': [1, 2, 3]}
     print_sorted_dictionary(a_dictionary)
 
 
