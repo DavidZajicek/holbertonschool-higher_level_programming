d1c432b5bced93e60ecc5d9d07f58ce4bd843b2d
David Zajicek <david.a.zajicek@outlook.com>
2024-09-11 12:21:56 +1000

    2-replace_in_list.py

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
+
+
+def main():
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    element = 10
+    print("{}".format(replace_in_list(my_list, idx, element)))
+
+
+if __name__ == "__main__":
+    main()
