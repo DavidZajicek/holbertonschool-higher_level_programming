558e2dfebd526282ebe8b0aa8beb69ebe7399a21
David Zajicek <david.a.zajicek@outlook.com>
2024-09-11 13:31:01 +1000

    negatives

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def max_integer(my_list: list = []):
-    pointer = 0
     if len(my_list) == 0:
         return None
+    pointer = my_list[0]
     for i in my_list:
         if i > pointer:
             pointer = i
@@ -11,7 +11,7 @@ def max_integer(my_list: list = []):
 
 
 def main():
-    my_list = [-1]
+    my_list = [-1, -2]
     max_value = max_integer(my_list)
     print("Max: {}".format(max_value))
 
