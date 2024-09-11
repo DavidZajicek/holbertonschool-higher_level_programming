2825e00bb6e0e0f7193a254fa4ebff02a72a4386
David Zajicek <david.a.zajicek@outlook.com>
2024-09-11 13:54:23 +1000

    don't return None

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-def delete_at(my_list: list = [], idx:int = 0):
+def delete_at(my_list: list = [], idx: int = 0):
     if idx < 0 or idx >= len(my_list):
-        return None
+        return my_list
     new_list: list = my_list.copy()
     my_list.clear()
     i = 0
