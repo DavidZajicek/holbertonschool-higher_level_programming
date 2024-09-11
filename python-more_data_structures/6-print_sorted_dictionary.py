4b7647092f397cc3af953fb88af177b1ef68475e
David Zajicek <david.a.zajicek@outlook.com>
2024-09-11 16:08:11 +1000

    6-print_sorted_dictionary.py

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary: dict):
+    for key in sorted(a_dictionary.keys()):
+        print("{}: {}".format(key, a_dictionary[key]))
+
+
+def main():
+    a_dictionary = {'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3]}
+    print_sorted_dictionary(a_dictionary)
+
+
+if __name__ == "__main__":
+    main()
