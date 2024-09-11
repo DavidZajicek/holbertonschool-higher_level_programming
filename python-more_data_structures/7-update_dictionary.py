f13f7064d98680aa7dd4bb500e586c36a1ab8695
David Zajicek <david.a.zajicek@outlook.com>
2024-09-11 16:19:41 +1000

    7-update_dictionary.py

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary: dict, key, value):
+    a_dictionary.update({key: value})
+    return a_dictionary
+
+
+def main():
+    a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
+    update_dictionary(a_dictionary, 'language', "Python")
+
+if __name__ == "__main__":
+    main()
