5c68c6d8501401f48699ac9e5aa61aa9dad0a667
David Zajicek <david.a.zajicek@outlook.com>
2024-09-11 13:47:38 +1000

    lint

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def divisible_by_2(my_list:list=[]):
+def divisible_by_2(my_list: list = []):
     new_list: list = []
     for i in my_list:
         if i % 2 == 0:
@@ -13,11 +13,7 @@ def divisible_by_2(my_list:list=[]):
 def main():
     my_list = [0, 1, 2, 3, 4, 5, 6]
     list_result = divisible_by_2(my_list)
-
-    i = 0
-    while i < len(list_result):
-        print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-        i += 1
+    print("Done {}", list_result)
 
 
 if __name__ == "__main__":
