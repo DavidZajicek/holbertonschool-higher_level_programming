58fc9351604e7b033eb12e4f34f1a38d1c979fe0
David Zajicek <david.a.zajicek@outlook.com>
2024-09-11 15:50:17 +1000

    So... do as I say not as I do?

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    return { c for c in set_1 if c in set_2 }
+    return {c for c in set_1 if c in set_2}
 
 
 def main():
-    set_1 = { "Python", "C", "Javascript" }
-    set_2 = { "Bash", "C", "Ruby", "Perl" }
+    set_1 = {"Python", "C", "Javascript"}
+    set_2 = {"Bash", "C", "Ruby", "Perl"}
     c_set = common_elements(set_1, set_2)
     print(sorted(list(c_set)))
 
