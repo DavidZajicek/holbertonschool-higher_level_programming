7e1b5f5e4545b0514949d9ff2c2d13313a31d615
David Zajicek <david.a.zajicek@outlook.com>
2024-09-15 10:51:05 +1000

    0-add_integer.py

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+"""
+0-add_integer
+This is a module for adding integers together using python3.8
+add_integer(a, b=98)
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Add two integers together
+    """
+    try:
+        _a = int(a)
+    except TypeError as err:
+        raise TypeError("a must be an integer") from err
+    try:
+        _b = int(b)
+    except (TypeError, ValueError) as err:
+        raise NameError("b must be an integer") from err
+    return _a + _b
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
