34a59b1fa7e3dd49bc66b81d00d4cf4af28e9d3b
David Zajicek <david.a.zajicek@outlook.com>
2024-09-15 11:41:36 +1000

    3-say_my_name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+2-matrix_divided
+This is a module for dividing all of the elements
+in a matrix by the given divisor
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    say_my_name prints the given names
+    """
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    print("{}".format(first_name), end=" ")
+    print(last_name)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
