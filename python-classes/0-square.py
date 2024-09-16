faba5933d794094d8933d40413ac9f7414d3f8d9
David Zajicek <david.a.zajicek@outlook.com>
2024-09-16 10:14:34 +1000

    0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+0-square
+Write an empty class Square that defines a square:
+
+You are not allowed to import any module
+"""
+
+
+class Square:
+    """
+    An empty class Square that defines a Square
+    """
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
