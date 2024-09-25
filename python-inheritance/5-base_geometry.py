9b300de398e0d3de1f1d4aceb76b05e642e499e9
David Zajicek <david.a.zajicek@outlook.com>
2024-09-25 10:56:45 +1000

    5-basegeo

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+5. Geometry module
+mandatory
+Write an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Base Geometry class (if you're naming it base, you're doing it wrong)
+    """
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
