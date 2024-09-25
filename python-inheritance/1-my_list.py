d74ef82d713b12184337aad66ceaf1779fd69704
David Zajicek <david.a.zajicek@outlook.com>
2024-09-25 10:18:13 +1000

    1-my_list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+1. My list
+mandatory
+Write a class MyList that inherits from list:
+"""
+
+
+class MyList(list):
+    """
+    MyList inherits from list
+    """
+
+    def print_sorted(self):
+        """
+        print list sorted
+        """
+        print(sorted(self))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
