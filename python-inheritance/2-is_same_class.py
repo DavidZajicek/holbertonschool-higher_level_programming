6b9654d112cebd05bd306c4f24d2f16190d02332
David Zajicek <david.a.zajicek@outlook.com>
2024-09-25 10:34:19 +1000

    2-is_same_class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+2. Exact same object
+mandatory
+Write a function that returns True if the object is exactly an instance of
+the specified class ; otherwise False.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    check is same class
+    """
+    return isinstance(obj, a_class)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
