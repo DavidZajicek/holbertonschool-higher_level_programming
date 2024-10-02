a24508afb2a0fb572c96612b6e3f240e0de026b1
David Zajicek <david.a.zajicek@outlook.com>
2024-10-02 11:20:26 +1000

    10-student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+9. Student to JSON
+mandatory
+Write a class Student that defines a student by:
+"""
+
+
+class Student():
+    """
+    Student
+    """
+
+    def __init__(self, first_name, last_name, age) -> None:
+        """
+        init tho
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Return self as serialisable json dict
+        """
+        if attrs:
+            return [x for x in self.__dict__ if x in attrs]
+        return self.__dict__
+
+
+if __name__ == "__main__":
+    pass
