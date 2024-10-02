35a453ebe9b66d05b0f102fc976ac104f442542d
David Zajicek <david.a.zajicek@outlook.com>
2024-10-02 11:11:29 +1000

    8-class_to_json

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+8. Class to JSON
+mandatory
+Write a function that returns
+"""
+
+
+def class_to_json(obj):
+    return obj.__dict__
+
+
+if __name__ == "__main__":
+    pass
