eccc3d77a9a87a0e0f2388164bd0276fdf34355e
David Zajicek <david.a.zajicek@outlook.com>
2024-10-09 12:51:23 +1100

    task_02_csv

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+0. Read file
+mandatory
+Write a function that reads a text file (UTF8) and prints it to stdout:
+"""
+import csv
+import json
+
+
+def convert_csv_to_json(csv_file):
+    data = {}
+
+    try:
+        with open(csv_file, encoding='utf-8') as csvfile:
+            csv_reader = csv.DictReader(csvfile)
+
+            for row in csv_reader:
+                key = row['name']
+                data[key] = row
+    except FileNotFoundError:
+        return False
+
+    with open('data.json', 'w', encoding='utf-8') as jsonfile:
+        jsonfile.write(json.dumps(data))
+
+    return True
+
+
+if __name__ == "__main__":
+    csv_file = "data.csv"
+    convert_csv_to_json(csv_file)
+    print(f"Data from {csv_file} has been converted to data.json")
