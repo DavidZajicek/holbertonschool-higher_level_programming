54a5cffb9e8066cd9204483c180170f22e3892bb
David Zajicek <david.a.zajicek@outlook.com>
2024-10-23 12:25:36 +1100

    7-citie4s

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,10 @@
 -- create user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS cities(
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
