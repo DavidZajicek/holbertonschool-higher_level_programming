55df44a788c4025a6dfcbb23c4785af26543271d
David Zajicek <david.a.zajicek@outlook.com>
2024-11-13 10:52:13 +1100

    **

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -35,7 +35,7 @@ def generate_invitations(template: str, attendees: [dict]):
 
     for attendee in attendees:
         try:
-            output = template.format_map(Attendee(attendee))
+            output = template.format_map(Attendee(**attendee))
             if os.path.exists(f"output_{index}.txt"):
                 raise FileExistsError
             with open(f"output_{index}.txt", 'w') as file:
@@ -47,20 +47,19 @@ def generate_invitations(template: str, attendees: [dict]):
         # print(output)
 
 
-# if __name__ == "__main__":
-#     # Read the template from a file
-#     with open('template.txt', 'r') as _file:
-#         template_content = _file.read()
+if __name__ == "__main__":
+    # Read the template from a file
+    with open('template.txt', 'r') as _file:
+        template_content = _file.read()
 
-#     # List of attendees
-#     _attendees = [
-#         {"name": "Alice", "event_title": "Python Conference",
-#             "event_date": "2023-07-15", "event_location": "New York"},
-#         {"name": "Bob", "event_title": "Data Science Workshop",
-#             "event_date": "2023-08-20", "event_location": "San Francisco"},
-#         {"name": "Charlie", "event_title": "AI Summit",
-#             "event_date": None, "event_location": "Boston"}
-#     ]
+    # List of attendees
+    _attendees = [
+        {"name": "Alice", "event_title": "Python Conference",
+            "event_date": "2023-07-15", "event_location": "New York"},
+        {"name": "Bob", "event_title": "Data Science Workshop",
+            "event_date": "2023-08-20", "event_location": "San Francisco"},
+        {}
+    ]
 
-#     # Call the function with the template and attendees list
-#     generate_invitations(template_content, _attendees)
+    # Call the function with the template and attendees list
+    generate_invitations(template_content, _attendees)
