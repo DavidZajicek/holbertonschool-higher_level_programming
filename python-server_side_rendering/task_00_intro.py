fe013d8d8258e9f1be119f62e47ad71d5d46de4b
David Zajicek <david.a.zajicek@outlook.com>
2024-11-13 10:41:17 +1100

    breaking it?

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -4,25 +4,20 @@
 mandatory
 Write a function that generates invitations
 """
-import json
 import os
 import sys
-import requests
-import csv
-
-
-class Attendee(dict):
-    def __missing__(self, key):
-        return f"{key}: N/A"
-
-    def __getitem__(self, key):
-        return super().__getitem__(key) or f"{key}: N/A"
 
 
 def generate_invitations(template: str, attendees: [dict]):
     """
     Generate Invitations
     """
+    class Attendee(dict):
+        def __missing__(self, key):
+            return f"{key}: N/A"
+
+        def __getitem__(self, key):
+            return super().__getitem__(key) or f"{key}: N/A"
     index = 0
     if not isinstance(template, str):
         print(f"Expected: str Received: {type(template)}")
@@ -32,6 +27,7 @@ def generate_invitations(template: str, attendees: [dict]):
         sys.exit()
     if len(attendees) < 1:
         print("No data provided, no output files generated.")
+        return
     if not isinstance(attendees[0], dict):
         print("Attendees data must be a list of dictionaries")
     if template == "":
